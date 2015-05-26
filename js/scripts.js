89c08ca6fd1b22c2b8ec1dd2db34547cce97e424
david garber & jocely alsdorf <student@epicodus.com>
2015-05-26 14:17:17 -0700

    adds jquery user interface funcitonality; web app working as expected

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,29 @@ var triangleTracker = function(side1, side2, side3) {
     return "equilateral";
   } else if ((side1 === side2) || (side2 === side3) || (side1 === side3)) {
     return "isoceles";
+  } else {
+    return "scalene";
   }
-    else {
-      return "scalene";
-    }
 
 };
+
+$(document).ready(function() {
+  $("form#tri").submit(function(event) {
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+
+    if(triangleTracker(side1, side2, side3) === "not a triangle") {
+      $("#not").show();
+      $("#result").hide();
+    } else {
+      $("#result").show();
+      $(".triangle").text(triangleTracker(side1, side2, side3));
+    }
+    event.preventDefault();
+  });
+
+
+
+
+});
